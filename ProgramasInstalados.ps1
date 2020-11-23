d7be162409cb8c8555213417a8e5e1235b459d4e
Helio <mm.helio009@gmail.com>
2020-11-23 08:36:23 -0400

    Proyecto inicial. Script en powershell para determinar el software instalado en una maquina y resultado lo agrega a un archivo TXT y asi mismo el archivo es almacenado en una carpeta destino

---
+++ ProgramasInstalados.ps1
@@ -0,0 +1,19 @@
+﻿
+$NombreEquipo= HOSTNAME
+$DireccionIP= ipconfig | Select-String -Pattern "IPv4" |% {($_ -replace "`r","") -replace "`n",""}
+$DireccionIP=$DireccionIP.Substring($DireccionIP.IndexOf(":")+1, $DireccionIP.Length - $DireccionIP.IndexOf(":")-1)
+$fecha= date |% {($_ -replace "`r","") -replace "`n",""}
+#$rutaArchivoDeResultado="C:\Users\hmejia\Desktop\SoftwareInstalado\$DireccionIP.txt"
+$rutaArchivoDeResultado="C:\Users\hmejia\Desktop\SoftwareInstalado\$DireccionIP.txt"
+echo "==============================================================================================================================" >> $rutaArchivoDeResultado
+echo "Nombre de equipo: "$NombreEquipo >> $rutaArchivoDeResultado
+echo  "Dirrecion de red: " $DireccionIP >> $rutaArchivoDeResultado
+echo "Ejecutado en fecha: "$fecha >> $rutaArchivoDeResultado
+echo "==============================================================================================================================" >> $rutaArchivoDeResultado
+Get-WmiObject -Class Win32_Product | Select-Object -Property name, version,Description >> $rutaArchivoDeResultado
+Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName,DisplayVersion | Format-Table –AutoSize >> $rutaArchivoDeResultado
+
+echo "------------------------------------------------------------------------------------------------------------------------------" >> $rutaArchivoDeResultado
+
+
+#powershell.exe -ExecutionPolicy Bypass -File C:\Users\hmejia\Desktop\ProgramasInstalados.ps1
